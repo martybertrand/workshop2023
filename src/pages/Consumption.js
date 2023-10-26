2e340a05b9db3038d5a4bc0afdf029d8b8a8e56c
martybertrand <marty.bertrand@hotmail.com>
2023-10-26 11:02:45 +0200

    axios config

--- src/pages/Consumption.js
+++ src/pages/Consumption.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ventil from '../img/ventil.png'
 import { Switch } from 'antd';
+import axios from 'axios'
 
 export default function Consumption() {
 
     const temp = 'rounded-2xl bg-white p-4 shadow-lg w-full'
     const btnTemp = 'bg-gray-600 text-white font-bold py-2 px-8 rounded-full hover:bg-gray-500'
 
-    const [ appartment, setAppartment ] = useState([ 18, 21, 22, 22, 21, 21, 19, 23, 18, 20, 23, 23, 23, 21, 20, 21, 21, 19, 19, 22 ])
+    const [ appartment, setAppartment ] = useState([])
 
     const onChange = (checked) => {
         console.log(`switch to ${checked}`);
     };
+
+    useEffect(() => {
+        axios.get('https://api-green-chauffage-n58glv11t-calvincs-projects.vercel.app/appartements')
+        .then((res) => res.data)
+        .then((data) => {
+            setAppartment(data)                             
+        })
+    }, [])
 
     return (
         <div className='flex flex-col p-8 leading-normal w-full'>
@@ -20,15 +29,15 @@ export default function Consumption() {
             <div className='grid grid-cols-4 w-full h-full gap-2'>
                 {
                     appartment.map((appart, index) => (
-                        <div key={index} className={temp}>
+                        <div key={appart.id} className={temp}>
                             <div className='flex justify-between'>
-                                <span className='font-bold'>Appartement {index + 1}</span>
+                                <span className='font-bold'>Appartement {appart.num_appartement}</span>
                                 <Switch defaultChecked onChange={onChange} />
                             </div>
 
                             <div className='flex w-full justify-between items-center h-full'>
                                 <div className='flex flex-col justify-center items-center'>
-                                    <span className='text-5xl font-bold'>{appart}</span>
+                                    <span className='text-5xl font-bold'>{appart.temperature_choisie} °C</span>
                                     <div>
                                         <button className={btnTemp} onClick={() => {
                                             const newAppartment = [...appartment]
